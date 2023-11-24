fd6770a9cec9d36f7971fd6b5c1a513ad81cdc28
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-24 17:34:01 +0530

    Custom fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,19 @@ class MyApp extends StatelessWidget {
               Text(
                 'Keshav Kumar',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Code Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               ),
